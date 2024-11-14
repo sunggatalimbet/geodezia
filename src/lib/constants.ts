04165692832eb234943afe9f094fe77b381161c2
Sunggat Alimbetov <sunggatalimbetov@gmail.com>
2024-11-14 16:51:25 +0500

    feat: images

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,39 +1,59 @@
+import service1 from "../images/service-1.png";
+import service2 from "../images/service-2.png";
+import service3 from "../images/service-3.png";
+import service4 from "../images/service-4.png";
+import service5 from "../images/service-5.png";
+import service6 from "../images/service-6.png";
+import serviceMore1 from "../images/service-more-1.png";
+import serviceMore2 from "../images/service-more-2.png";
+import serviceMore3 from "../images/service-more-3.png";
+import serviceMore4 from "../images/service-more-4.png";
+import serviceMore5 from "../images/service-more-5.png";
+import serviceMore6 from "../images/service-more-6.png";
+import serviceMore7 from "../images/service-more-7.png";
+import serviceMore8 from "../images/service-more-8.png";
+import serviceMore9 from "../images/service-more-9.png";
+import serviceMore10 from "../images/service-more-10.png";
+import serviceMore11 from "../images/service-more-11.png";
+import serviceMore12 from "../images/service-more-12.png";
+import result from "../images/results-1.png";
+
 export const servicesTriangles = [
 	{
 		text: "Геологические изыскания",
-		imgSrc: "src/lib/images/service-1.png",
+		imgSrc: service1.src,
 		imgClip: "polygon(100% 50%, 0 0, 0 100%)",
 		textPos: "absolute top-0 -right-10 md:right-0",
 	},
 	{
 		text: "Экологические изыскания",
-		imgSrc: "src/lib/images/service-2.png",
+		imgSrc: service2.src,
 		imgClip: "polygon(0 50%, 100% 0, 100% 100%)",
 		textPos:
 			"absolute top-1/2 transform -translate-y-1/2 -right-[80px] md:-right-[120px]",
 	},
 	{
 		text: "Гидрометерологические изыскания",
-		imgSrc: "src/lib/images/service-3.png",
+		imgSrc: service3.src,
 		imgClip: "polygon(100% 50%, 0 0, 0 100%)",
 		textPos: "absolute -bottom-4 md:bottom-0 -right-8",
 	},
 	{
 		text: "Техническое обследование зданий, сооружений",
-		imgSrc: "src/lib/images/service-4.png",
+		imgSrc: service4.src,
 		imgClip: "polygon(100% 0, 0 50%, 100% 100%)",
 		textPos: "absolute -top-6 -left-12 md:left-0",
 	},
 	{
 		text: "Разработка проекта С33",
-		imgSrc: "src/lib/images/service-5.png",
+		imgSrc: service5.src,
 		imgClip: "polygon(100% 50%, 0 0, 0 100%)",
 		textPos:
 			"absolute top-1/2 transform -translate-y-1/2 -left-[80px] md:-left-[120px]",
 	},
 	{
 		text: "Геодезичские изыскания",
-		imgSrc: "src/lib/images/service-6.png",
+		imgSrc: service6.src,
 		imgClip: "polygon(100% 100%, 0 50%, 100% 0)",
 		textPos: "absolute -bottom-4 md:bottom-0 md:left-0",
 	},
@@ -41,73 +61,73 @@ export const servicesTriangles = [
 
 export const services = [
 	{
-		imageSrc: "src/lib/images/service-more-1.png",
+		imageSrc: serviceMore1.src,
 		title: "предпроектной бурение в составе геологических изысканий",
 		subtitle:
 			"Георадарные исследования: цели, задачи и нюансы При желании качественно выполнить обследование объекта, следует заказать такую услугу, как георадарная съемка − один из наиболее распространенных, помехозащищенный…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-2.png",
+		imageSrc: serviceMore2.src,
 		title: "⁠⁠проектирование и подготовительные работы",
 		subtitle:
 			"Профессионально выполненные инженерно-геологические изыскания для строительства – это основа безошибочного проектирования дома и любого другого сооружения.",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-3.png",
+		imageSrc: serviceMore3.src,
 		title: "бурение скважин и анализ грунта",
 		subtitle:
 			"Компания ООО «ГеоГИС» осуществляет специальные геофизические изыскания, цена которых вас приятно удивит. Геофизические работы являются обязательной составной частью геологических изысканий для строительства…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-4.png",
+		imageSrc: serviceMore4.src,
 		title: "отчет подсчета запасов на основание разведки",
 		subtitle:
 			"Началу строительно-монтажных и любых земляных работ предшествует изучение условий на местности. Абсолютно одинаковых участков нет. Каждая площадка имеет свои особенности и индивидуальные характеристики.…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-5.png",
+		imageSrc: serviceMore5.src,
 		title: "разработка тэо разведочных кондиций",
 		subtitle:
 			"Топографическая съемка – наиболее востребованный вид услуг, оказываемых специалистами нашего профильного предприятия «ГеоГИС» населению и организациям Москвы, Московского региона по выгодной цене. Посмотреть…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-6.png",
+		imageSrc: serviceMore6.src,
 		title: "геодезическое сопровождение строительства",
 		subtitle:
 			"Геодезическое сопровождение строительства – это проводимый специалистами, оговоренный в нормативно-правовых актах, комплекс изысканий. Он необходим для сбора и анализа всех данных об объекте строительства,…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-7.png",
+		imageSrc: serviceMore7.src,
 		title: "инжинерно-экологические изыскания для строительства",
 		subtitle:
 			"Экологические изыскания представляют собой комплекс мероприятий, направленных на оценку влияния планируемой деятельности на окружающую среду. Эти исследования проводятся с целью выявления возможных негативных…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-8.png",
+		imageSrc: serviceMore8.src,
 		title: "инжинерно-гидрометеорологические изыскания",
 		subtitle:
 			"Квалифицированные специалисты нашей компании «ГеоГИС» по заявке клиента выполняют инженерные изыскания с целью получения максимально полной информации о территории. Наряду с исследованиями геологии, экологии…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-9.png",
+		imageSrc: serviceMore9.src,
 		title: "разработка проекта сзз",
 		subtitle:
 			"Промышленные предприятия, как крупные, так и малые, оказывают значительное влияние на окружающую среду. В условиях современной застройки нередки случаи, когда дома, парки, скверы и другие места пребывания…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-10.png",
+		imageSrc: serviceMore10.src,
 		title: "экспертиза промышленной безопасности",
 		subtitle:
 			"На промышленных объектах задействованы разные постройки, механизмы и техника для обеспечения нормального функционирования предприятия, а также выполнения плановых показателей. Из-за того, что на таких…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-11.png",
+		imageSrc: serviceMore11.src,
 		title: "инженерно-технические изыскание",
 		subtitle:
 			"Перед началом строительства необходимо провести анализ территории будущей постройки. Для этого осуществляют инженерно-технические изыскания. Это такие изыскания, задача которых состоит в выяснении, насколько…",
 	},
 	{
-		imageSrc: "src/lib/images/service-more-12.png",
+		imageSrc: serviceMore12.src,
 		title: "техническое обследование зданий и оборудования",
 		subtitle:
 			"'ГеоГИС' предлагает полный набор услуг по контролю, тестированию и оценке состояния объектов. Техническое обследование зданий и сооружений представляет собой комплексную оценку текущего состояния данных…",
